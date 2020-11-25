f397ce407da7e4b8f7318c09b366ecc1fe47f80f
Hugo J <hjbello.wk@gmail.com>
2020-11-25 14:24:19 +0100

    clean

--- clean_up_space.sh
+++ clean_up_space.sh
@@ -1,5 +1,9 @@
-docker stop $(docker ps -a -q)
-docker rm $(docker ps -a -q)
+#docker stop $(docker ps -a -q)
+#docker rm $(docker ps -a -q)
+docker stop news-scraper-workers-daily-go
+docker stop ews-scraper-workers-historic-go
+docker rm news-scraper-workers-daily-go
+docker rm ews-scraper-workers-historic-go
 docker volume prune -f
 docker container prune -f
 docker-compose up -d
