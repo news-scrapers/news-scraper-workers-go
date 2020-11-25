dfdc5ddc7142c95a207cec69708785e14ad32c32
Hugo J <hjbello.wk@gmail.com>
2020-11-25 14:25:47 +0100

    clean

--- clean_up_space.sh
+++ clean_up_space.sh
@@ -1,7 +1,7 @@
 #docker stop $(docker ps -a -q)
 #docker rm $(docker ps -a -q)
 docker stop news-scraper-workers-daily-go
-docker stop ews-scraper-workers-historic-go
+docker stop news-scraper-workers-historic-go
 docker rm news-scraper-workers-daily-go
 docker rm ews-scraper-workers-historic-go
 docker volume prune -f
